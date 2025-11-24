f07897b3272e8e96697094a6bd9c0d5b0fe5ba9b
Alejandro_Carlos <90822568+Alcardro@users.noreply.github.com>
2025-11-23 19:58:34 -0500

    Url backend

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 // src/lib/api.ts
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://gestiondecasosbackend-production.up.railway.app/api';
 
 // Interfaces para la API
 export interface LoginRequest {
